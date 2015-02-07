2906d04394e852eaf284c43dc4b6542d8072de74
zzo-MAC <reumia@hyeonseog-ui-MacBook-Pro.local>
2015-02-07 19:14:40 +0900

    JSON 출력 이니셜

--- js/common.js
+++ js/common.js
@@ -1,3 +1,38 @@
-/**
- * Created by reumia on 15. 2. 7..
- */
+$(function(){
+
+    $.getJSON( "js/data.json", function( data ) {
+
+        var items = data['환율정보'],
+            $tbody = $("<tbody></tbody>");
+
+        for( var i in items ){
+            var $tr, $th, $span, $td_std, $td_comp, $td_per;
+
+
+            $th = $("<th></th>", {
+                html: items[i]['국가']
+            }).appendTo($tr);
+            $span = $("<span></span>", {
+                class: "text-default",
+                html: " " + items[i]['통화명']
+            }).appendTo($th);
+            $td_std = $("<td></td>", {
+                class: "td-std",
+                html: items[i]['매매기준율']
+            }).appendTo($tr);
+            $td_comp = $("<td></td>", {
+                class: "td-comp",
+                html: items[i]['전일대비']
+            }).appendTo($tr);
+            $td_per = $("<td></td>", {
+                class: "td-per",
+                html: items[i]['등락율']
+            }).appendTo($tr);
+            $tr.appendTo($tbody);
+        }
+
+        $tbody.appendTo('.cal-table');
+
+    });
+
+});
